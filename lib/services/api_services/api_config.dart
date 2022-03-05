eb8b9fb596b13eb0b947f2aae1349f7b46befc79
ashraf459810 <78394984+ashraf459810@users.noreply.github.com>
2022-03-05 12:30:37 +0300

    done

--- lib/services/api_services/api_config.dart
+++ lib/services/api_services/api_config.dart
@@ -1,10 +1,10 @@
 import 'package:get_storage/get_storage.dart';
 import 'package:shopizy/settings/storage_keys.dart';
 
-const String baseUrl = 'https://dev-api.shopizyy.com/api';
-// const String baseUrl = 'https://api.shopizi.co/api';
-const String webBaseUrl = 'https://dev-web.shopizyy.com';
-// const String webBaseUrl = 'https://shopizi.co';
+// const String baseUrl = 'https://dev-api.shopizyy.com/api';
+const String baseUrl = 'https://api.shopizi.co/api';
+// const String webBaseUrl = 'https://dev-web.shopizyy.com';
+const String webBaseUrl = 'https://shopizi.co';
 const String cdnBaseUrl = 'https://shopizy-17ca8.kxcdn.com';
 const String notificationsUrl = 'https://noti.shopizyy.com/api/notifications';
 
